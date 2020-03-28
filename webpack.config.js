9bca1fca51bd310213759e3b7ef28ca143b59940
Kohei Seta <ktarou78@gmail.com>
2020-03-28 17:26:29 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = [{
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].js',
+    filename: 'js/[name].[hash].js',
     // publicPath: '/',
   },
   devServer: {
